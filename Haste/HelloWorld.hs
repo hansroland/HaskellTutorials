186d73a77d0d5fc65c8c53d879b8c1fffa1a508c
roland <rsx@bluewin.ch>
2014-12-25 11:51:51 +0100

    Hello World in Haste

---
+++ Haste/HelloWorld.hs
@@ -0,0 +1,14 @@
+-- ------------------------------------------------------------------------
+-- HelloWorld.hs - First and very basic Haste example
+-- ------------------------------------------------------------------------
+-- 
+-- See: http://www.cse.chalmers.se/edu/year/2014/course/TDA555/haste.html
+--
+-- Compile with: hastec --output-html HelloWorld.hs
+--
+-- Open the resulting HTML-File in the browser
+-- -------------------------------------------------------------------------
+
+import Haste
+
+main = alert "Hello world!"
