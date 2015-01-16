9fc7a65cd8d47df8f236e1d29a1bc39d8377674f
roland <rsx@bluewin.ch>
2015-01-16 10:52:04 +0100

    Some cleanup during a reread of the article

--- Transformers/Transformers.hs
+++ Transformers/Transformers.hs
@@ -31,6 +31,7 @@ data Exp = Lit Integer        -- ^ Integer literals
          | App Exp Exp        -- ^ Function applications
          deriving (Show)
 
+
 -- | Values (aka Results of our program
 data Value = IntVal Integer   -- ^ Integer values
            | FunVal Env Name Exp  -- ^ Functions (closures)
@@ -38,6 +39,23 @@ data Value = IntVal Integer   -- ^ Integer values
 
 -- | Environments in which a function is evaluated (= variable dictionary)
 type Env = Map.Map Name Value
+
+-- --------------------------------------------------------------------------
+-- Examples
+-- --------------------------------------------------------------------------
+
+-- | A simple example to test our different evaluators
+exampleExp :: Exp
+exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
+
+-- | A bad example: x is not defined
+badExp :: Exp
+badExp = (Plus (Lit 1) (Abs "x" (Var "x")))
+
+-- | Undefined variable
+undefExp :: Exp
+undefExp = (Var "x")
+
 
 ---------------------------------------------------------------------
 -- The Non-Monadic version of the interpreter
@@ -61,20 +79,15 @@ eval0 env (App e1 e2) =
     in case val1 of
         FunVal env' n body -> eval0 (Map.insert n val2 env') body
 
--- | A simple example to test our different evaluators
-exampleExp :: Exp
-exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
-
--- | A bad example: x is not defined
-badExp :: Exp
-badExp = (Plus (Lit 1) (Abs "x" (Var "x")))
-
--- | Undefined variable
-undefExp :: Exp
-undefExp = (Var "x")
 
 -- >>> eval0 Map.empty exampleExp
 -- IntVal 18
+
+-- >>> eval0 Map.empty badExp
+-- -> ECEPTION: Irrefutable Pattern
+
+-- >>> eval0 Map.empty undefExp
+-- -> EXCEPTION: Maybe.fromJust: Nothing
 
 --------------------------------------------------------------------
 -- The Most-Simple-Monadic version of the interpreter
@@ -102,8 +115,14 @@ eval1 env (App e1 e2) = do
     case val1 of
         FunVal env' n body -> eval1 (Map.insert n val2 env') body
 
--- >>> runEval1 (eval1 Map.empty exampleExp)
+-- >>> runEval1 $ eval1 Map.empty exampleExp
 -- IntVal 18
+
+-- >>> runEval1 $ eval1 Map.empty badExp
+-- -> EXCEPTION: Pattern Match failure
+
+-- >>> runEval1 $ eval1 Map.empty undefExp
+-- --> EXCEPTION: undef variable: x
 
 -- ----------------------------------------------------------------
 -- Adding Error Handling: Use the ErrorT Transformer
@@ -135,19 +154,20 @@ eval2a env (App e1 e2) = do
     case val1 of
         FunVal env' n body -> eval2a (Map.insert n val2 env') body
 
-
--- >>> runEval2 (eval2a Map.empty exampleExp)
+-- >>> runEval2 $ eval2a Map.empty exampleExp 
 -- Right (IntVal 18)
 
--- >>> runEval2 (eval2a Map.empty badExp)
--- Gives a failure
+-- >>> runEval2 $ eval2a Map.empty badExp
+-- Left "Pattern match failure in do expression at Transformers.hs:137:5-13"
+
+-- Left "undef variable: x"
 
 -- | Modified eval2a to use the error reporting of the errorT Transformer
 eval2b :: Env -> Exp -> Eval2 Value
 eval2b env (Lit i) = return $ IntVal i
 eval2b env (Var n) = maybe (fail ("undef variable: " ++ n)) 
        return $ Map.lookup n env
-eval2b env (Plus e1 e2) = do    -- Improved to check the evaluation results 
+eval2b env (Plus e1 e2) = do    -- Improved to check the evaluation results
     e1' <- eval2b env e1
     e2' <- eval2b env e2
     case (e1', e2') of
@@ -161,11 +181,31 @@ eval2b env (App e1 e2) = do
         FunVal env' n body -> eval2b (Map.insert n val2 env') body
         _                  -> throwError "type Error"    -- Added !!
 
--- >>> runEval2 (eval2b Map.empty badExp)
--- Left "type error"
-
 -- >>> runEval2 (eval2b Map.empty exampleExp)
--- Right (IntVal 18)
+-- -> Right (IntVal 18)
+
+-- >>> runEval2 (eval2b Map.empty badExp)
+-- -> Left "type error"
+
+-- >>> runEval2 $ eval2a Map.empty undefExp
+-- -> Left "undef variable: x"
+
+
+-- Code eval2c does not compile !!
+{-
+eval2c :: Env -> Exp -> Eval2 Value
+eval2c env (Lit i) = return $ IntVal i
+eval2c env (Var n) = Map.lookup n env
+eval2c env (Plus e1 e2) = do     -- <---- changed !!
+    IntVal i1 <- eval2c env e1
+    IntVal i2 <- eval2c env e2
+    return $ IntVal (i1 + i2)
+eval2c env (Abs n e) = return $ FunVal env n e
+eval2c env (App e1 e2) = do
+    FunVal env' n body <- eval2c env e1
+    val2 <- eval2c env e2
+    eval2c (Map.insert n val2 env') body
+-}
 
 
 -- | The final version for  evaluation with error messages
@@ -201,7 +241,7 @@ eval2 env (App e1 e2) = do
 -- Hiding the Environment
 -- ---------------------------------------------------------------------------
 
--- | Take Eval2 andadd a ReaderT at the beginning
+-- | Take Eval2 and add a ReaderT at the beginning
 type Eval3 a = ReaderT Env (ErrorT String Identity) a
 
 -- | The run function for the type Eval3
