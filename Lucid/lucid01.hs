99b8b4a6b4d993dfebd101a759ef5c4a0e29f5da
roland <rsx@bluewin.ch>
2015-05-15 14:09:10 +0200

    Use renderToFile instead of LucidExt

--- Lucid/lucid01.hs
+++ Lucid/lucid01.hs
@@ -5,11 +5,12 @@
 -- See: http://jaspervdj.be/blaze/tutorial.html
 --      http://chrisdone.com/posts/lucid
 --      https://github.com/chrisdone/lucid
+--      http://chrisdone.com/posts/lucid2
 --
 -- -------------------------------------------------------------------------
 -- {-# LANGUAGE OverloadedStrings #-}
 
-import LucidExt
+import Lucid
 import Control.Monad (mapM_)
 
 numbers :: Monad m => Int -> HtmlT m ()
@@ -19,3 +20,5 @@ numbers n = doctypehtml_ $ do
     body_ $ do
        p_ $ toHtml "A list of natural numbers"
        ul_ $ mapM_ (li_ . toHtml . show) [1..n]
+
+-- use renderToFile "berg21.html" $ numbers 7
