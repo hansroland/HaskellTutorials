fa9b734c1a744f29e5781f0f1e5faa332b2e3b76
roland <rsx@bluewin.ch>
2016-03-11 15:19:47 +0100

    Tutorial 3

---
+++ Servant/Tutorial3.hs
@@ -0,0 +1,113 @@
+-- -----------------------------------------------------------------
+-- A Servant Example with arguments
+-- ----------------------------------------------------------------
+--
+-- This corresponds to tutorial 3
+--
+-- See:
+--    https://haskell-servant.github.io/tutorial/server
+--
+-- ----------------------------------------------------------------
+
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Main where
+
+import Control.Monad.Trans.Either
+import Data.Aeson
+import GHC.Generics
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+import Servant.API
+
+import Data.List(intercalate)
+
+-- --------------------------------------------------------------------
+-- Data Types
+-- -------------------------------------------------------------------
+data Position = Position
+  { x :: Int
+  , y :: Int
+  } deriving Generic
+
+instance ToJSON Position
+
+newtype HelloMessage = HelloMessage { msg :: String }
+  deriving Generic
+
+instance ToJSON HelloMessage
+
+data ClientInfo = ClientInfo
+  { clientName :: String
+  , clientEmail :: String
+  , clientAge :: Int
+  , clientInterestedIn :: [String]
+  } deriving Generic
+
+instance FromJSON ClientInfo
+instance ToJSON ClientInfo
+
+data Email = Email
+  { from :: String
+  , to :: String
+  , subject :: String
+  , body :: String
+  } deriving Generic
+
+instance ToJSON Email
+
+emailForClient :: ClientInfo -> Email
+emailForClient c = Email from' to' subject' body'
+
+  where from'    = "great@company.com"
+        to'      = clientEmail c
+        subject' = "Hey " ++ clientName c ++ ", we miss you!"
+        body'    = "Hi " ++ clientName c ++ ",\n\n"
+                ++ "Since you've recently turned " ++ show (clientAge c)
+                ++ ", have you checked out our latest "
+                ++ intercalate ", " (clientInterestedIn c)
+                ++ " products? Give us a visit!"
+
+-- | API definition
+type API = "position" :> Capture "x" Int :> Capture "y" Int :> Get '[JSON] Position
+      :<|> "hello" :> QueryParam "name" String :> Get '[JSON] HelloMessage
+      :<|> "marketing" :> ReqBody '[JSON] ClientInfo :> Post '[JSON] Email
+
+-- | Handler definitons
+server3 :: Server API
+server3 = position
+      :<|> hello
+      :<|> marketing
+
+-- The position handler
+position :: Int -> Int -> EitherT ServantErr IO Position
+position x y = return (Position x y)
+
+hello :: Maybe String -> EitherT ServantErr IO HelloMessage
+hello mname = return . HelloMessage $ case mname of
+    Nothing -> "Hello, anonymous coward"
+    Just n  -> "Hello, " ++ n
+
+marketing :: ClientInfo -> EitherT ServantErr IO Email
+marketing clientinfo = return (emailForClient clientinfo)
+
+-- --------------------------------------------------------
+-- Boilerplate for plumbing up this to a WebServer
+-- --------------------------------------------------------
+userAPI :: Proxy API
+userAPI = Proxy
+
+app1 :: Application
+app1 = serve userAPI server3
+
+main :: IO()
+main = run 8081 app1
+
+-- How to call
+-- http://localhost:8081/position/15/16
+-- http://localhost:8081/hello?name=Roland
+-- curl -X POST -d '{"name":"Alp Mestanogullari", "email" : "alp@foo.com", "age": 25, "interested_in": ["haskell", "mathematics"]}' -H 'Accept: application/json' -H 'Content-type: application/json' http://localhost:8081/marketing
