ebf7fa458b5591c4754e78eb45f5181ad46684cf
roland <rsx@bluewin.ch>
2016-04-16 22:16:02 +0200

    Add  second handler with own page

--- Scotty/src/DirectoryViewer.hs
+++ Scotty/src/DirectoryViewer.hs
@@ -9,17 +9,44 @@ import Directories
 import Data.Text
 import Lucid
 
+-- |
 main = do
   dirs <- subdirs ""
-  scotty 3000 $
-    get "/" . html . renderText $ do
-      doctype_
-      myPage1 $ fmap pack dirs
+  scotty 3000 $ routes $ fmap pack dirs
 
-myPage1 :: [Text] -> Html ()
-myPage1 ar = do
+-- | The routing table
+routes :: [Text] -> ScottyM ()
+routes dirs = do
+  get "/"                 $ showParent dirs
+  get "/dir/:dirName" $ do
+    dir <- param "dirName"
+    showSubdir dir
+
+-- | Handler when no parameter is given -> show first page
+showParent :: [Text] -> ActionM ()
+showParent dirs =
+     html $ renderText $ page1 dirs
+
+-- | Handler when a dir parameter is given
+--   http://localhost:3000/dir/test
+showSubdir :: Text -> ActionM ()
+showSubdir dir =
+    html $ renderText $ page2 dir
+
+-- | Show the first page
+page1 :: [Text] -> Html ()
+page1 ar = do
       head_ $
         title_ "Title"
       body_ $ do
         h1_ "Directories"
         ul_ (mapM_ (li_ . toHtml ) ar)
+
+-- | Show the second page
+page2 :: Text -> Html ()
+page2 dir = do
+  head_ $
+    title_ "Title"
+  body_ $ do
+    h1_ "Page 2 "
+    h2_ $ toHtml dir
