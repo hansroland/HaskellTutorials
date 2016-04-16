1e6a156170b710cfba95b26779e3461eb415a641
roland <rsx@bluewin.ch>
2016-04-16 18:56:10 +0200

    Change DirectoryViewer to Lucid and show really a directory

--- Scotty/src/DirectoryViewer.hs
+++ Scotty/src/DirectoryViewer.hs
@@ -1,37 +1,25 @@
 -- -------------------------------------------------------------------------
--- A first Scotty program to set up the project
+-- A first Scotty program to show some directories
 -- -------------------------------------------------------------------------
 
 {-# LANGUAGE OverloadedStrings #-}
+
 import Web.Scotty
-
 import Directories
-import Control.Monad.Trans (liftIO)
-import Control.Monad
+import Data.Text
+import Lucid
 
-import Text.Blaze.Html.Renderer.Text
+main = do
+  dirs <- subdirs ""
+  scotty 3000 $
+    get "/" . html . renderText $ do
+      doctype_
+      myPage1 $ fmap pack dirs
 
-import qualified Data.Text.Lazy as T
-
--- import Text.Blaze.Html5.Attributes as A
-import Text.Blaze.Html5 as H hiding (main)
-
-main = scotty 3000 $
-  get "/" $ do
-    txt <- liftIO $ liftM  blazeDirs $ subdirs ""
-    Web.Scotty.html $ renderHtml txt
-
--- Note: liftM lifts the function blazeDirs into the IO monad
--- of the subdirs result
-
-htmlSubdirs :: [String] -> T.Text
-htmlSubdirs = T.pack . concat
-
-
-blazeDirs :: [String] -> Html
-blazeDirs as = H.docTypeHtml $ do
-    H.head $
-      H.title "Hello!"
-    H.body $ do
-      H.h1 "Title"
-      H.p ("Hello " >> toHtml (Prelude.head as) >> "!")
+myPage1 :: [Text] -> Html ()
+myPage1 ar = do
+      head_ $
+        title_ "Title"
+      body_ $ do
+        h1_ "Directories"
+        ul_ (mapM_ (li_ . toHtml ) ar)
